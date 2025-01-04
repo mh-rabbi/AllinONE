a262e2e3f2c9b9602b1ea9ea1b819709653cce9c
mh-rabbi <mhr221official@gmail.com>
2025-01-04 12:39:24 +0600

    design updated

--- app/src/main/java/com/demoapp/multiverse/MainActivity.java
+++ app/src/main/java/com/demoapp/multiverse/MainActivity.java
@@ -10,15 +10,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] predefinedUrls = {
+            "https://www.nytimes.com",         // The New York Times
+            "https://www.theguardian.com",    // The Guardian
+            "https://www.wsj.com",            // The Wall Street Journal
+            "https://www.washingtonpost.com", // The Washington Post
+            "https://www.bbc.com/news",       // BBC News
+            "https://www.cnn.com",            // CNN
+            "https://www.aljazeera.com",      // Al Jazeera
+            "https://www.reuters.com"         // Reuters
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
